4602016476fe826d9488b299e1dff296184e4b15
Carsten <carsten.hohnke@gmail.com>
2023-05-04 15:26:20 -0400

    Pandas

--- analyze_reviews.py
+++ analyze_reviews.py
@@ -1,1 +1,22 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the CSV file and set the type of the date column
+reviews = pd.read_csv("branch_reviews.csv", index_col=0, parse_dates=["date"])
+
+# Display a histogram of the number of reviews by date
+reviews["date"].hist()
+#plt.show()
+
+# Create a new dataframe from reviews with the average rating for each date
+reviews_by_date = reviews.groupby("date").mean()
+
+# Plot a 30-day moving average rating per date
+reviews_by_date[90:].rolling(30).mean().plot()
+#plt.show()
+
+# Print the number of reviews by rating
+print(reviews["rating"].value_counts())
+
+# Describe reviews
+print(reviews.describe())
